e63829b38f071250e343b2e7e37317faa519fe96
vitoupen <pen.vitou@gmail.com>
2019-11-25 13:29:34 -0800

    Change Search Bar

--- Android/WeConnect/app/src/main/java/com/cs5540/weconnect/homepage/HomeFragment.kt
+++ Android/WeConnect/app/src/main/java/com/cs5540/weconnect/homepage/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.cs5540.weconnect.homepage
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 
@@ -25,6 +29,25 @@ class HomeFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
             R.layout.fragment_home, container, false)
 
+        binding.searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            //as the new character is added(can be used to generate suggestions)
+            override fun onQueryTextChange(newText: String): Boolean {
+                Toast.makeText(activity,newText,Toast.LENGTH_SHORT).show()
+                return true
+            }
+            //click  submit
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //TO DO navigate to the result page
+                Toast.makeText(activity,query,Toast.LENGTH_LONG).show()
+
+                // Hide the keyboard.
+                val inputMethodManager =
+                    binding.searchBar.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+                return true
+            }
+        })
         // Inflate the layout for this fragment
         return binding.root
     }
