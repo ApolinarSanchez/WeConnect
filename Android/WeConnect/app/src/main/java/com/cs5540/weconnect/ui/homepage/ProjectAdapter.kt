d5e36e34cf910ed4ceb4c0486a70a3d89a27b3e1
vitoupen <pen.vitou@gmail.com>
2019-12-02 20:30:29 -0800

    Vitou (#6)
    * remove unused ui folders, make changes to gradle files, ActivityMain, HomeFragment and their layouts
    
    * started on Homepage layout
    
    * make it works now
    
    * newest commit
    
    * added categories to home layout
    
    * fixed layout and wrapped imageButtons in cardviews and removed background shadow drawables
    
    * vcs.xml update
    
    * Revert "fixed layout and wrapped imageButtons in cardviews and removed background shadow drawables"
    
    This reverts commit 922f8b627d0f2171e5661157ce9ad0849da21503.
    
    * Change Search Bar
    
    * Change search bar
    
    * added back the ui folder
    
    * fix error in layout
    
    * add recycler view with dummy data
    
    * added scrollable category view
    
    * completes the recycler views with dummy data
    
    * remove a space
    
    * nothing new
    
    * nothing
    
    * change navigation.xml

---
+++ Android/WeConnect/app/src/main/java/com/cs5540/weconnect/ui/homepage/ProjectAdapter.kt
@@ -0,0 +1,33 @@
+package com.cs5540.weconnect.ui.homepage
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cs5540.weconnect.R
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+class ProjectAdapter(val catList: ArrayList<ProjectModel>): RecyclerView.Adapter<ProjectAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name?.text = catList[position].name
+        holder.img?.setImageResource(catList[position].imgId)
+        holder.description?.text=(catList[position].description)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.project_item_layout, parent, false)
+        return ViewHolder(v);
+    }
+
+    override fun getItemCount(): Int {
+        return catList.size
+    }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val name = itemView.findViewById<TextView>(R.id.project_name)
+        val description = itemView.findViewById<TextView>(R.id.project_description)
+        val img = itemView.findViewById<ImageView>(R.id.project_image)
+    }
+
+}
---
+++ Android/WeConnect/app/src/main/java/com/cs5540/weconnect/ui/homepage/ProjectModel.kt
@@ -0,0 +1,4 @@
+package com.cs5540.weconnect.ui.homepage
+
+data class ProjectModel(var name: String, var imgId: Int, var description: String) {
+}
